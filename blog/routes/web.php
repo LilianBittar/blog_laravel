df4c07de3e26235c56e65dbb12d359cbf9ccc55d
LilianBittar <lilian.bittar@pm.me>
2024-04-08 08:12:00 +0200

    Extracted a dropdown blade component.

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -8,9 +8,10 @@ use App\Models\User;
 
 Route::get('/', function () {
   return view('posts', [
-    'posts' => Post::latest()->get()
+    'posts' => Post::latest()->get(),
+    'categories' => Category::all()
   ]);
-});
+})->name('home');
 
 
 Route::get('posts/{post:slug}', function (Post $post) {
@@ -26,7 +27,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
