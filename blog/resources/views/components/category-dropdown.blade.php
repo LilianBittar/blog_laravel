b03e5a860b3993a7e60c8df2e2aafe659b6be67f
LilianBittar <lilian.bittar@pm.me>
2024-04-09 21:33:24 +0200

    Merged category and search queries.

--- blog/resources/views/components/category-dropdown.blade.php
+++ blog/resources/views/components/category-dropdown.blade.php
@@ -13,10 +13,10 @@
 
     @foreach ($categories as $category)
         <x-dropdown-item 
-            href="/?category={{ $category->slug }}" 
+            href="/?category={{ $category->slug }}${{ http_build_query(request()->except('category')) }}"
             :active="request()->is('categories/' . $category->slug)">
             {{ ucwords($category->name) }}
         </x-dropdown-item>
      
     @endforeach
-</x-dropdown>
+</x-dropdown> 
