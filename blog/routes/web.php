27945cef76e811201440bdac773875deda0e5586
LilianBittar <lilian.bittar@pm.me>
2024-04-09 18:45:18 +0200

    Author filtering.

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,9 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
+
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
@@ -12,8 +10,3 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts
-    ]);
-}); 
