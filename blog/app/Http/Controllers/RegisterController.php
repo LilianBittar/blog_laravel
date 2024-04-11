5ee33a466438c4557dba3e3016385b74a4554ff4
LilianBittar <lilian.bittar@pm.me>
2024-04-11 16:57:29 +0200

    Build a register page.

---
+++ blog/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => 'required',
+            'username' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:7|max:255'
+        ]);
+
+
+        $attributes['password'] = bcrypt(request('password'));
+        User::create($attributes);
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
+
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,3 +12,7 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+
+Route::get('register', [RegisterController::class, 'create']);
+
+Route::post('register', [RegisterController::class, 'store']);
