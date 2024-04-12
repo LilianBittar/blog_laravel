3b6aede4766dd1447d9c331acc83423f81da2b94
LilianBittar <lilian.bittar@pm.me>
2024-04-12 16:16:44 +0200

    table consistency and foreign key constraints.

--- blog/database/migrations/2024_03_31_165044_create_post_table.php
+++ blog/database/migrations/2024_03_31_165044_create_post_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
                 $table->id();
+                $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+                $table->foreignId('category_id')->constrained()->cascadeOnDelete();
                 $table->string('slug')->unique();
                 $table->string('title');
                 $table->text('excerpt');
